8b08f643d81820831ff7e49519a8e5adab236fcc
silvanshade <silvanshade@users.noreply.github.com>
2024-03-20 18:39:43 -0600

    WIP

--- packages/milkv/pioneer/bsp/default.nix
+++ packages/milkv/pioneer/bsp/default.nix
@@ -2,7 +2,7 @@
 
 lib.makeScope pkgs.newScope (self: {
   edk2 = self.callPackage ./edk2.nix { inherit flake; };
-  # linux = self.callPackage ./linux.nix { inherit flake; };
+  linux = self.callPackage ./linux.nix { inherit flake; };
   opensbi = self.callPackage ./opensbi.nix { inherit flake; };
   zsbl = self.callPackage ./zsbl.nix { inherit flake; };
 })
